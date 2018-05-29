972db1dd8044eba2db8a61c161e6b3d11f9b1757
lcarranco <lcarranco@gmail.com>
2018-05-28 22:06:30 -0500

    Added find_all function
    Added sum_between function

--- main.cpp
+++ main.cpp
@@ -3,6 +3,23 @@
 
 using namespace std;
 
+int find_all(vector<int> steps) {
+    for (int i = 0; i < steps.size(); ++i) {
+        while (steps[i] == 0) {
+
+        }
+    }
+    return 0;
+}
+
+int sum_between(int left, int right, vector<int> steps) {
+    int sum = 0;
+    for (int i = left + 1; i < right; ++i) {
+        sum += steps[i];
+    }
+    return sum;
+}
+
 int main() {
     int n = 0;
     string s;
@@ -27,11 +44,15 @@ int main() {
             steps.push_back(++k);
         }
     }
-    for (vector<int>::iterator it = steps.begin(); it != steps.end(); ++it) {
-        cout << *it << ' ';
-    }
+    // for (vector<int>::iterator it = steps.begin(); it != steps.end(); ++it) {
+    //     cout << *it << ' ';
+    // }
 
-    
+    int left = 0;
+    int right = 0;
+    vector<int> sums;
+    find_all(steps);
+    sums.push_back(sum_between(left, right, steps));
 
     return 0;
 }
