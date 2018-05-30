274cbac285dbd53914a6ef30db3413e195635cb7
lcarranco <lcarranco@gmail.com>
2018-05-30 17:00:57 -0500

    Removed find_all and sum_between functions

--- main.cpp
+++ main.cpp
@@ -3,25 +3,6 @@
 
 using namespace std;
 
-void find_all(vector<int> steps, vector<int> & indices) {
-    // cout << steps.size();
-    for (int i = 0; i < steps.size(); ++i) {
-        if (steps[i] == 0) {
-            indices.push_back(i);
-            // cout << i << ' ';
-        }
-    }
-}
-
-int sum_between(int left, int right, vector<int> steps) {
-    int sum = 0;
-    for (int i = left + 1; i < right; ++i) {
-        sum += steps[i];
-    }
-    // cout << sum << endl;
-    return sum;
-}
-
 int main() {
     int n = 0;
     string s;
